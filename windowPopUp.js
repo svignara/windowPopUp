8fef196d2f041ae7c64611cf947725802044b12a
svignara <suvethan@gmail.com>
2013-01-09 23:39:07 -0500

    cleaning up script

--- windowPopUp.js
+++ windowPopUp.js
@@ -1,24 +1,43 @@
-var screenWidth = window.screen.availWidth
-  , screenHeight = window.screen.availHeight
-  ;
+var windowPopUp = ( function(){
 
+    return {
+        
+        init : function(){
+            
+            this.clickList();
+            
+        },
+        
+        clickListen : function() {
+            var screenWidth = window.screen.availWidth
+            , screenHeight = window.screen.availHeight
+            ;
 
-$('.windowPopUp').on('click', function(e){
+            $('.windowPopUp').on('click', function(e){
   
-  e.preventDefault();
-  
-  var element = $(this)
-    , url = element.attr('href')
-    , width = element.attr('data-popup-width')
-    , height = element.attr('data-popup-height')
-    , modalWidth = ( width > screenWidth ) ? screenWidth - 80 : width
-    , modalHeight = ( height > screenHeight ) ? screenHeight - 80 : height
-    , offsetLeft = (screenWidth - modalWidth) / 2
-    , offsetTop = (screenHeight - modalHeight) / 2
-    ;
+            e.preventDefault();
+            
+            var element = $(this)
+              , url = element.attr('href')
+              , width = element.attr('data-popup-width')
+              , height = element.attr('data-popup-height')
+              , modalWidth = ( width > screenWidth ) ? screenWidth - 80 : width
+              , modalHeight = ( height > screenHeight ) ? screenHeight - 80 : height
+              , offsetLeft = (screenWidth - modalWidth) / 2
+              , offsetTop = (screenHeight - modalHeight) / 2
+              ;
     
-  console.log(element, url, modalWidth, modalHeight, offsetTop, offsetLeft);
+                console.log(element, url, modalWidth, modalHeight, offsetTop, offsetLeft);
   
-  window.open(url,'windowpopup','width='+ modalWidth +',height='+ modalHeight +',left='+ offsetLeft +',top='+ offsetTop +',screenX='+ offsetLeft +',screenY='+ offsetTop);
+                window.open(url,'windowpopup','width='+ modalWidth +',height='+ modalHeight +',left='+ offsetLeft +',top='+ offsetTop +',screenX='+ offsetLeft +',screenY='+ offsetTop);
 
+            });
+        }
+        
+    }
+
+}() );
+
+$(document).ready(function(){
+    windowPopUp.init();
 });
